1d17c3c19f5d2e3404e7e257ae9753be3e66b096
Seabiscuit <hmkhan@qq.com>
2017-12-17 16:27:51 +0800

    net461 => net471

--- nuspecs/pack.cmd
+++ nuspecs/pack.cmd
@@ -1,4 +1,4 @@
-@set NUGET_PACK_OPTS= -Version 4.0.0-rtm-170501
+@set NUGET_PACK_OPTS= -Version 4.0.0-rtm-171112
 @set NUGET_PACK_OPTS= %NUGET_PACK_OPTS% -OutputDirectory Publish
 
 %~dp0nuget.exe pack %~dp0Topshelf.nuspec %NUGET_PACK_OPTS%
