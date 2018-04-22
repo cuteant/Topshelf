d5b317ea5945302ee1797332108c39886e8e06ca
Seabiscuit <hmkhan@qq.com>
2018-04-22 20:58:42 +0800

    整理下项目

--- nuspecs/pack.cmd
+++
@@ -1,6 +0,0 @@
-@set NUGET_PACK_OPTS= -Version 4.0.0-rtm-171112
-@set NUGET_PACK_OPTS= %NUGET_PACK_OPTS% -OutputDirectory Publish
-
-%~dp0nuget.exe pack %~dp0Topshelf.nuspec %NUGET_PACK_OPTS%
-%~dp0nuget.exe pack %~dp0Topshelf.NLog.nuspec %NUGET_PACK_OPTS%
-%~dp0nuget.exe pack %~dp0Topshelf.Serilog.nuspec %NUGET_PACK_OPTS%
